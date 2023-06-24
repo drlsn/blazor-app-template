ec6ecfb83ea5f56c377f23fba76d5f4ea661f39d
drlsn <dariusz.lusnia@gmail.com>
2023-06-24 21:15:50 +0200

    Add command and query executo

---
+++ src/MyApp.UI.Common/Data/IQueryExecutor.cs
@@ -0,0 +1,22 @@
+﻿using Common.Basic.Blocks;
+using Mediator;
+
+namespace MyApp.UI.Common.Data
+{
+    public interface IQueryExecutor
+    {
+        Task<TResponse> Execute<TQuery, TResponse>(TQuery query, CancellationToken cancellationToken = default)
+            where TQuery : IQuery<Result<TResponse>>;
+
+        Task<TResponse> Execute<TQuery, TResponse>(CancellationToken cancellationToken = default) 
+            where TQuery : IQuery<Result<TResponse>>, new() 
+            => Execute<TQuery, TResponse>(new TQuery(), cancellationToken);
+
+        Task<TResponse> Execute<TQuery, TResponse>(string id, CancellationToken cancellationToken = default)
+            where TQuery : IQuery<Result<TResponse>>
+        {
+            var query = (TQuery) Activator.CreateInstance(typeof(TQuery), id);
+            return Execute<TQuery, TResponse>(query, cancellationToken);
+        }
+    }
+}
